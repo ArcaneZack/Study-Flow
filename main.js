469d6ea21f0a2a47c8cf269fdec56b5ee1f35ca0
Zachary Bowen <Zachary.Bowen1@SNHU.edu>
2025-11-21 00:34:44 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    icon: path.join(__dirname, 'icon.png') // optional
+  });
+
+  win.loadFile('index.html');
+  
+  // Remove menu bar (optional - cleaner look)
+  win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
